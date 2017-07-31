9e0cf73c05bd5a04954e50cb0469c2743eff78d9
Dominic <haakym@gmail.com>
2017-07-31 13:06:47 +0100

    lesson 21 Global Scopes and Further Query Reduction

--- app/Thread.php
+++ app/Thread.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     protected $guarded = [];
+    
+    protected $with = ['creator', 'channel'];
 
     protected static function boot()
     {
@@ -26,9 +28,7 @@ class Thread extends Model
 
     public function replies()
     {
-    	return $this->hasMany(Reply::class)
-            ->withCount('favourites')
-            ->with('owner');
+    	return $this->hasMany(Reply::class);
     }
 
     public function creator()
