ed1ae1c514d6c8bbd99c8956e868cbd2d526c868
Dominic <haakym@gmail.com>
2017-08-03 09:48:59 +0100

    lesson 24 Authorization With Policies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -9,15 +9,17 @@
                     <div class="level">
                         <span class="flex">
                             <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted: 
-                            {{ $thread->title }}
+                            <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
                         </span>
 
-                        <form action="{{ $thread->path() }}" method="POST">
-                            {{ csrf_field() }}
-                            {{ method_field('DELETE') }}
+                        @can ('update', $thread)
+                            <form action="{{ $thread->path() }}" method="POST">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
 
-                            <button class="btn btn-link" type="submit">Delete Thread</button>
-                        </form>
+                                <button class="btn btn-link" type="submit">Delete Thread</button>
+                            </form>
+                        @endcan
                     </div>
                 </div>
 
